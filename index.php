8c7503e4c5a7ff061f43dc80249d7d9a855e4af8
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-10-29 17:15:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-  echo "Witaj świecie nazywam się Patryk Sprus";
- ?>
+
+$conn = new mysqli("mysql-patryksprus.alwaysdata.net","217136","@H@R8He5TsaaXJR","patryksprus_baza");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+
+?>
