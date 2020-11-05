03dd4adf2a293df249199df09088b340d48f6fb1
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-05 23:36:13 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <div>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-spruspatryk/">Strona glowna</a><br/>
+    <a href="funkcAgregujace.php">funkcAgregujace.php</a><br/>
+    </div>
+
 <?php
-echo("teest: "funkcAgregujace.php");
+echo("Jestem tu: <br/>orgPracownicy.php<br/>");
 ?>
+  </body>
+</html>
