0dec1c37765b543df38fa0b49e063bf17534bf82
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-06 00:20:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-spruspatryk">GitHub</a>
     <h1>Patryk Sprus nr 11</h1>
 <div class="nav">
-    <a href="OrgPracownicy.php">Organizacja Pracownicy</a>
+    <a href="orgPracownicy">Organizacja Pracownicy</a>
     <a href="funkcAgregujace.php">Funkcje Agregujące</a>
 </div>
 </body>
