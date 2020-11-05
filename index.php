8ed91bbdb4bf9148839bc3a53bc657e1d067552d
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-05 14:05:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -29,6 +29,19 @@ $wynik = mysqli_query($conn, $sql);
     }
 
     echo('</table>');
+    echo ("Zad.2");
+    $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial and imie not like '%a' group by dzial;
+echo("<h3>zadanie1</h3>"); 
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['avg(zarobki)']."</td><td>".$row['nazwa_dzial']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
 
 
 ?>
