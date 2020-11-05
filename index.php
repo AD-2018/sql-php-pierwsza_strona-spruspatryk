676f94b5e7312f766bf0e40069a0bd8177661cc2
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-05 14:18:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $result = mysqli_query($conn, $sql);
     echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th>');
 
-    while($row=mysqli_fetch_assoc($wynik)){
+    while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
         echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
         echo('</tr>');
