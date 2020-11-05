de15179919f9797c1fe1d6979b0a6c5d2a9f7980
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-05 23:46:38 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -10,7 +10,30 @@
     </div>
 
 <?php
-echo("Jestem tu: <br/>orgPracownicy.php<br/>");
+  $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial";
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+         echo "<li>ok";
+     } else {
+       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+
+    echo("<h1>Zadanie 1</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
 ?>
   </body>
 </html>
