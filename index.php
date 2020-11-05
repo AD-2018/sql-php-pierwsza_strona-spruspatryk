32bfe9d519ae736bda9e0a953b456adfcd9cf024
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-06 00:08:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,49 +4,11 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-spruspatryk">Github</a><br/>
-    <a href="orgPracownicy.php">orgPracownicy.php</a><br/>
-    <a href="funkcAgregujace.php">funkcAgregujace.php</a><br/>
-    </div>
-  
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-spruspatryk">GitHub</a>
     <h1>Patryk Sprus nr 11</h1>
-   
-
-<?php
-
-$conn = new mysqli("mysql-patryksprus.alwaysdata.net","217136","@H@R8He5TsaaXJR","patryksprus_baza");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo ("Zad.1");
-$sql = "SELECT * FROM pracownicy";
-$result = mysqli_query($conn, $sql);
-
-
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-    echo ("Zad.2");
-    $sql ="select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial and imie not like '%a' group by dzial"; 
-$result = mysqli_query($conn, $sql);  
-echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>"); 
-while($row=mysqli_fetch_assoc($result)){ 
-  echo("<tr>");     
-  echo("<tr>");     
-  echo("<td>".$row['avg(zarobki)']."</td><td>".$row['nazwa_dzial']."</td>");     
-  echo("<tr>"); 
-  echo("</tr>"); } 
-echo('</table>'); 
-    
-?>
+<div class="nav">
+    <a href="OrgPracownicy.php">Organizacja Pracownicy</a>
+    <a href="agreg.php">Funkcje Agregujące</a>
+</div>
 </body>
 </html>
