1ce45e42a66463819dd5f7be6d1564f085596697
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-06 00:00:02 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -4,36 +4,55 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-     <div>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-spruspatryk/">Strona glowna</a><br/>
-    <a href="funkcAgregujace.php">funkcAgregujace.php</a><br/>
-    </div>
+        <div class="nav">
+    <a href="index.php">main</a>
+</div>
+</body>
+</html>
 
 <?php
-  $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial";
+require_once("connect.php");
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
+echo("<br>Zad 1 <br>");
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
+echo($sql);
 
-    $result = mysqli_query($conn, $sql);
-    if ( $result) {
-         echo "<li>ok";
-     } else {
-       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-     }
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 
-    echo("<h1>Zadanie 1</h1>");
-    echo("<h2>".$sql."</h2>");
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
 
-    echo("<table border='1'>");
-    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
-        while($row = mysqli_fetch_assoc($result)) {
-            echo("<tr>");
-            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
-            echo("</tr>");
-        };
-    echo("</table>");
-?>
-  </body>
-</html>
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+echo("<br>Zad 2 <br>");
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial and imie like '%a'";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
