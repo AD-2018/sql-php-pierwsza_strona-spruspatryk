7d90768743b8dbe039118e508d2a13fdfadbec75
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-06 12:10:32 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -13,7 +13,7 @@
 require_once("connect.php");
 
 echo("<br>Zad 1 <br>");
-$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
+$sql = "select nazwa_dzial,sum(zarobki) from pracownicy,organizacja where id_org=dzial";
 echo($sql);
 
 $result = mysqli_query($conn, $sql);
@@ -34,25 +34,6 @@ echo('<table border="1">');
 
     echo('</table>');
 
-echo("<br>Zad 2 <br>");
-$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial and imie like '%a'";
-echo($sql);
 
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
 
-echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
 ?>
