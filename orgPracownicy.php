27c1828b13fa20f7c5f989421d328090fb8f37e0
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-06 11:58:57 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -55,5 +55,27 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+
+echo("<br>Zad 3 <br>");
+$sql = "SELECT * FROM pracownicy, organizacja where imie not like '%a'";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
 
