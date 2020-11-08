f043c2fd7f61d9775320e2b684ccff3f01b184c0
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-08 14:12:58 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -268,5 +268,28 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+
+
+echo("<br>Zad 12</br>");
+ $sql ="SELECT * from pracownicy,organizacja where id_org=dzial order by data_urodzenia asc limit 0, 1"; 
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>Nazwa działu</th><th>Zarobki</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['nazwa_dzial'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
 
