8efd1f69b4073e0f93437f243c98b5b6912bf93a
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-08 12:49:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,3 +12,29 @@
 </div>
 </body>
 </html>
+
+
+<?php
+require_once("connect.php");
+
+echo("<br>Zad 1 <br>");
+$sql = "SELECT * from pracownicy where dzial=2
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Dział</th><th>Imie</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['dzial'].'</td><td>'.$row['imie'].'</td>');
+        echo('</tr>');
+    }
+   echo('</table>');
+   ?>
