4e709a17c0d42794afd6259604d48cbea2dba9e6
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-08 13:30:25 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -174,6 +174,30 @@ echo('<table border="1">');
 
     echo('</table>');
 
+//---------------------------------
+
+echo("<br>Zad 8</br>");
+$sql ="SELECT * from pracownicy,organizacja where id_org=dzial and imie like '%a' having (dzial=1 or dzial=3) order by imie asc"; 
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>Nazwa działu</th><th>Zarobki</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['nazwa_dzial'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
 
 ?>
 
