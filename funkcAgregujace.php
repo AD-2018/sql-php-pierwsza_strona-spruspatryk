5cee32e6df7250e88092a9d9e0e6f08ad2230a4d
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-08 01:42:33 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -203,23 +203,7 @@ echo('<table border="1">');
     }
     echo('</table>');
 
-echo("<br>Zad 12<br>");
-$sql = "SELECT dzial,sum(zarobki) as średnia_zarobków FROM pracownicy, organizacja where id_org=dzial group by dzial";
-echo($sql);
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-echo('<table border="1">');
-    echo('<th>dzial</th><th>średnia zarobków</th>');
-    while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['dzial'].'</td><td>'.$row['średnia_zarobków'].'</td>');
-        echo('</tr>');
-    }
-    echo('</table>');
+
 
 
 echo("<br>Zad 12<br>");
