24db855e9513fe8a845a05860acd058215952a2d
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-12 14:05:30 +0100

    Update DataCzas.php

--- DataCzas.php
+++ DataCzas.php
@@ -56,4 +56,30 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+
+//-----------------------
+
+echo("<br>Zad 3<br>");
+$sql ="SELECT *,SUM(YEAR(CURDATE()) - YEAR(data_urodzenia)) as suma_lat from pracownicy,organizacja where id_org=dzial"; 
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Suma lat</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['suma_;at'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
+
+
