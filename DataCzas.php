dba390456310383b4291c3e0d7783249ed211f24
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-12 14:38:33 +0100

    Update DataCzas.php

--- DataCzas.php
+++ DataCzas.php
@@ -154,27 +154,7 @@ echo('<table border="1">');
     echo('</table>');
 //--------------------------
 
-echo("<br>Zad 7<br>");
-$sql ="SELECT *,avg(YEAR(CURDATE()) - YEAR(data_urodzenia)) as srednia_lat from pracownicy,organizacja where id_org=dzial"); 
-echo($sql);
 
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-
-echo('<table border="1">');
-    echo('<th>Średnia lat</th><th>Nazwa działu</th>');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['srednia_lat'].'</td>'.'<td>'.$row['nazwa_dzial'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
 ?>
 
 
