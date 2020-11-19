4e429e6458ddf05d91a19ef8c7f30d9a7f1579da
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-19 14:10:06 +0100

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
+       VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
+
+$conn->query($sql);
+
+$conn->close();
+?>
