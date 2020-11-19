eff646f327ae8e81b387b8e22e05ebb265b9fbd6
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-19 14:12:41 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$servername = "mysql-patryksprus.alwaysdata.net";
+$username = "217136";
+$password = "@H@R8He5TsaaXJR";
+$dbname = "patryksprus_baza";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
