85e4e27e76dc75da159cc66db4dbc474551fd2f6
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-19 14:38:40 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -9,8 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki) 
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
 $conn->query($sql);
