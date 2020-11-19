0c76de4e1eec3122536ee1d7c476bc8cda80e8d1
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-19 14:24:19 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
+$sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki) 
        VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
 $conn->query($sql);
