1eca408e675564bc1aa2b71c35fbe7566f0d07cd
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-25 19:43:37 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,4 +1,8 @@
+
 <?php
+echo("jestes w insert.php");
+echo "<li>".$_POST['dzial'];
+echo "<li>".$_POST['imie'];
 
 $servername = "mysql-patryksprus.alwaysdata.net";
 $username = "217136";
@@ -9,10 +13,13 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "INSERT INTO pracownicy (dzial, imie) 
-       VALUES (3, Ksawery)";
 
-$conn->query($sql);
+$sql = "INSERT INTO pracownicy (dzial, imie) 
+       VALUES (null, '".$_POST['dzial']."', '".$_POST['imie']."';
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 ?>
