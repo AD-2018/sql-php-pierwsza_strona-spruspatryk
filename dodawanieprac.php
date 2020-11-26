4431ee3cd4ddaec9e5a6365e77b6da1069f2b53e
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-11-26 14:52:42 +0100

    Update dodawanieprac.php

--- dodawanieprac.php
+++ dodawanieprac.php
@@ -38,7 +38,7 @@ $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial";
     echo("<h1>Tabela</h1>");
 
     echo("<table border='1'>");
-    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzialu</th>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dział</th><th>Nazwa działu</th>");
         while($row = mysqli_fetch_assoc($result)) {
             echo("<tr>");
             echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
