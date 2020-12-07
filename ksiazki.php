2e4906f00c9ad417ddbfa0a9dfd67f11f32f9293
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-12-07 14:52:33 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -1,36 +1,31 @@
+  
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" href="style.css">
-  <title>Książki</title>
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div>
-<a href="https://sprus-patryk.herokuapp.com">Strona Glowna </a>
-</div>
+  
 <?php
-require_once "connect.php";
+require_once("connect.php");
 
-$sql = "SELECT * FROM bibl_autor";
-echo("<h2>Autorzy</h2>");
-echo("<li>".$sql."<br><br>");
-
+echo("<br>zad 1<br>");
+$sql = "SELECT * FROM biblAutor";
+echo($sql);
 $result = mysqli_query($conn, $sql);
-     if ( $result) {
+if ( $result) {
         echo "<li>ok";
     } else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
-
-echo('<table border="1" class="tabelka_moja">');
-echo ("<tr><th>id_autor</th><th>autor</th></tr>");
-while($row = mysqli_fetch_assoc($result)) {
-    echo ('<tr>');
-    echo ("<td>".$row['id_autor']."</td><td>".$row['autor']."</td>");
-    echo ('</tr>');
-}
-echo ('</table>');
+echo('<table border="1">');
+    echo('<th>Id</th><th>Autor</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id'].'</td><td>'.$row['autor'].'</td>');
+        echo('</tr>');
+    }
+    echo('</table>');
+?>
 </body>
 </html>
