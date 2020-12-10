cf0fb8acf6de88164f703db087bf2ac43f3a1266
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-12-10 13:39:49 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -26,6 +26,24 @@ echo('<table border="1">');
         echo('</tr>');
     }
     echo('</table>');
+  
+  echo("<br>zad 2<br>");
+$sql = "SELECT * FROM biblTytul";
+echo($sql);
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<table border="1">');
+   echo('<th>Id</th><th>Tytuł</th>');
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id'].'</td><td>'.$row['tytul'].'</td>');
+        echo('</tr>');
+    }
+    echo('</table>');
 ?>
 </body>
 </html>
