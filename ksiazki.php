e95a5050b4aa1725a3e65a7d6964804720bd0438
PatrykSprus <73642584+PatrykSprus@users.noreply.github.com>
2020-12-17 13:18:56 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -8,6 +8,48 @@
   
 <?php
 require_once("connect.php");
+  
+   echo ("<br>autorzy<br>");
+$sql = "SELECT * FROM bibl_autor";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<select name="Autor">');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo'<option value="'.$row['id_autor'].'">';
+        echo($row['autor']);
+        echo"</option>"; 
+    }
+echo('</select>');
+  echo("<br>");
+  
+  echo ("<br>tytuł<br>");
+$sql = "SELECT * FROM bibl_tytul";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
+
+echo('<select name="Tytuł">');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo'<option value="'.$row['id_tytul'].'">';
+        echo($row['tytul']);
+        echo"</option>"; 
+    }
+echo('</select>');
+  echo("<br>");
 
 echo("<br>zad 1<br>");
 $sql = "SELECT * FROM biblAutor";
