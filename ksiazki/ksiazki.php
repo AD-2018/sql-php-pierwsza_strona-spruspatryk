ad9f16b996a139ac9a62cc398c4c62deb8593864
PatrykSprus <sprus1.patryk@gmail.com>
2021-02-07 15:50:01 +0100

    1

--- ksiazki/ksiazki.php
+++ ksiazki/ksiazki.php
@@ -53,6 +53,27 @@ echo('</select>');
   
   echo("<br>lista<br>");
   $sql = "select (`biblAutor_biblTytul`.id) as ID_TAB, autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where biblAutor.id=biblAutor_id and biblTytul.id=biblTytul_id order by autor,ID_TAB asc";
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+  $result = mysqli_query($conn, $sql);
+  if ( $result) {
+   } else {
+     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+   }
+  echo("<table border='1' class='tab_bibl'>");
+  echo("<th>ID</th><th>Autor</th><th>Tytuł</th><th>Wypożyczona</th>");
+      while($row = mysqli_fetch_assoc($result)) {
+          echo("<tr>");
+          echo("<td>".$row['ID_TAB']."</td><td>".$row['autor']."</td><td>".$row['tytul']."</td><td>");
+    if ( $row['biblWypoz'] == "0") {
+      echo("NIE</td>");
+    } else {
+      echo("TAK</td>");
+    };
+          echo("</tr>");
+      };
+  echo("</table>");  
 $result = mysqli_query($conn, $sql);
 if ( $result) {
         echo "<li>ok";
