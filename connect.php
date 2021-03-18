49eeda537a4ea453ede5b672c80dcabdad26b2df
PatrykSprus <sprus.patryk1@gmail.com>
2021-03-18 13:53:49 +0100

    vbox

--- connect.php
+++ connect.php
@@ -1,10 +1,16 @@
-  
 <?php
+$hostname = $_SERVER['HTTP_HOST'];
+if ($hostname=="localhost"){
+  $servername = 'localhost';
+  $username = 'user';
+  $password = 'a';
+  $dbname = 'user'; 
+} else {
 $servername = "mysql-patryksprus.alwaysdata.net";
 $username = "217136";
 $password = "@H@R8He5TsaaXJR";
 $dbname = "patryksprus_baza";
-
+}
 $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
--- index.php
+++ index.php
@@ -23,6 +23,46 @@
 <?php
 require_once("connect.php");
 
+echo("<h1>Tabelka Pracownicy</h1>");
+
+    
+$sql = "INSERT INTO pracownicy (imie,dzial,zarobki) 
+   VALUES ('Patryk','1','21')";
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+     echo "<br>";
+ } else {
+   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+ }    
+ $sql = "SELECT * from pracownicy";
+ echo($sql);
+ 
+ $result = mysqli_query($conn, $sql);
+ if ( $result) {
+         echo "<li>ok";
+     } else {
+       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+ 
+echo("<table border='1'>");
+echo("<th>Imie</th><th>Zarobki</th><th>Dzial</th>");
+    while($row = mysqli_fetch_assoc($result)) {
+        echo("<tr>");
+        echo("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['dzial']."</td>");
+        echo("</tr>");
+    };
+echo("</table>");
+echo ("<br>");
+
+ echo("<h2>Czas</h2>");
+echo (date("Y-m-d h:i:sa", $d));
+echo("<br>");
 echo("<br>Zad 1 <br>");
 $sql = "SELECT * from pracownicy where dzial=2";
 echo($sql);
