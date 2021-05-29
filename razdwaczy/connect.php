3c8b32294092b979a512cb5257877a3bf6f8f058
PatrykSprus <sprus1.patryk@gmail.com>
2021-05-29 23:11:00 +0200

    asdasd

---
+++ razdwaczy/connect.php
@@ -0,0 +1,7 @@
+<?php
+$servername = 'remotemysql.com';
+$username = 'IhtENaerut';
+$password = 'mpC2yX116m';
+$dbname = 'IhtENaerut';
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
