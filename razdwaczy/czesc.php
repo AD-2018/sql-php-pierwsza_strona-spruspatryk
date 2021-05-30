c67ee7da4d48b537252615293b63c708fb734040
PatrykSprus <sprus1.patryk@gmail.com>
2021-05-30 22:08:30 +0200

    1

--- razdwaczy/czesc.php
+++ razdwaczy/czesc.php
@@ -1,29 +1,18 @@
-<!DOCTYPE html>
-<link rel="stylesheet" href="style.css">
-<div class="grid_uklad">
-<div class="gridA">
-<?php
 
-require_once("connect.php");
-$sql = "SELECT Name from country where Continent = 'Africa'";
-$result = mysqli_query($conn, $sql);
-echo("<table border='1'>");
-echo("<th>Kraj</th><th>kontynent</th>");
-while($row = mysqli_fetch_assoc($result))
+
+<?php
+require_once("connect.php")
+$sql = "SELECT Name from country where Continent = 'Africa'"
+$result = mysqli_query($conn,$sql);
+echo("table border='1'");
+echo("<th>Kraj</th><th><Kontynent</th>");
+while($row= mysqli_fetch_assoc($result))
 {
     echo("<tr>");
-    echo("<td>".$row['Name']."</td><td>".$row['Continent']."</td>");
+    eco("<td>".$row['Name']."</td><td>".$row['Continent']."</td>");
     echo("</tr>");
+
 };
-    echo("</table>");
-    echo("</br>");
+echo("</table>");
+echo("</br>");
 ?>
-</div>
-        <div class="gridB"></div>
-        <div class="gridC"></div>
-        <div class="gridD"></div>
-        <div class="gridE"></div>
-        </div>
-
-
-</html>
