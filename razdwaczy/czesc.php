15997260152b0cb319a34de7a1b08b6b052d9af4
PatrykSprus <sprus1.patryk@gmail.com>
2021-05-30 21:41:15 +0200

    1

--- razdwaczy/czesc.php
+++ razdwaczy/czesc.php
@@ -2,7 +2,7 @@
 <?php
 
 require_once("connect.php");
-$sql = "SELECT * from country where continent = 'Africa'";
+$sql = "SELECT Name from country where Continent = 'Africa'";
 $result = mysqli_query($conn, $sql);
 echo("<table border='1'>");
 echo("<th>Kraj</th><th>kontynent</th>");
@@ -15,4 +15,3 @@ while($row = mysqli_fetch_assoc($result))
     echo("</table>");
     echo("</br>");
 ?>
-</html>
