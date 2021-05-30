5f2778056bdc4dc872f67f01f85062cca455411a
PatrykSprus <sprus1.patryk@gmail.com>
2021-05-30 15:06:56 +0200

    1

--- razdwaczy/czesc.php
+++ razdwaczy/czesc.php
@@ -6,12 +6,13 @@ $sql = "SELECT name from country where Region = 'Caribbean'";
 $result = mysqli_query($conn,$sql);
 echo("<tabler border='1'>");
 echo("<th>Kraj</th><th>region</th>");
-while($row = mysqli_fetch_assoc($result));
+while($row = mysqli_fetch_assoc($result))
 {
     echo("<tr>");
-    echo("<td>".$row['Name']."<td><td>".$row['Region']."</td>");
+    echo("<td>".$row['Name']."</td><td>".$row['Region']."</td>");
     echo("</tr>");
 };
 echo("</table>");
 echo("<br>");
 ?>
+
