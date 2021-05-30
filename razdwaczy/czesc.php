cb90982e44c48fccf6eae7820f7b2dbe92fe311f
PatrykSprus <sprus1.patryk@gmail.com>
2021-05-30 14:55:56 +0200

    1

--- razdwaczy/czesc.php
+++ razdwaczy/czesc.php
@@ -1,20 +1,17 @@
-<?php    
-
-			
-require_once("connect.php");
-$sql = "SELECT Name from country where  Region = 'Caribbean'";
-$result = mysqli_query($conn,$sql);
-echo("<table border='1'>");
-echo("<th>Kraj</th><th>Region</th>");
-while($row = mysqli_fetch_assoc($result)){
-    echo("<tr>");
-    echo("<td>".$row['Name']."<td><td>".$row['Region']."</td>");
-    echo("<tr>");
-};
-echo("/table>");
-echo("<br>");
-?>
 
 	
-
+<?php
+require_once("connect.php");
+$sql = "select name from country where region = 'Caribbean'";
+$result = mysqli_query($conn,$sql);
+echo("<th>Kraj</th><th>region</th>");
+while($row = mysqli_fetch_assoc($result));
+{
+    echo("<tr>");
+    echo("<td>".$row['Name']."</td><td>".$row['Region']."</td>");
+    echo("</tr>")
+};
+echo("</table>")
+echo("<br>");
+?>
 
