1c165b313b360d162dd66bdf3d4d2c9331b66710
PatrykSprus <sprus1.patryk@gmail.com>
2021-06-01 21:37:55 +0200

    1

--- razdwaczy/czesc.php
+++ razdwaczy/czesc.php
@@ -1,18 +1,15 @@
-
-
 <?php
 require_once("connect.php");
-$sql = "SELECT * FROM klienci where imie not like '%a'";
-$result = mysqli_query($conn, $sql);
-
-echo("<h1>Tabelka</h1>");
+$sql = "SELECT avg(koszt) as sredniacena from produkty";
+$result = mysqli_query($conn,$sql);
 echo("<table border='1'>");
-echo("<th>id_klient</th><th>Imie</th><th>nazwisko</th><th>Data Urodzenia</th><th>kraj</th>");
-while($row = mysqli_fetch_assoc($result)) {
-echo("<tr>");
-echo("<td>".$row['id_klient']."</td><td>".$row['imie']."</td><td>".$row['nazwisko']."</td><td>".$row['data_urodzenia']."</td><td>".$row['kraj']."</td>");
-echo("</tr>");
-};
+echo("<th>średnia cena</th>");
+while($row=mysqli_fetch_assoc($result))
+{
+    echo("<tr>");
+    echo("<td>".$row['koszt']."</td>");
+    echo("</tr>");
+
+}
 echo("</table>");
-echo ("<br>");
 ?>
