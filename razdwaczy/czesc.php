5d144db7991e58b3277d1cb48e7081ad23489b85
PatrykSprus <sprus1.patryk@gmail.com>
2021-06-01 22:00:16 +0200

    1

--- razdwaczy/czesc.php
+++ razdwaczy/czesc.php
@@ -1,6 +1,6 @@
 <?php
 require_once("connect.php");
-$sql = "SELECT sum(ilosc) from koszyk where imie like '%a'";
+$sql = "SELECT sum(ilosc) from koszyk,klienci,zamowienia where zamowienia.id_klient=klienci.id_klient and zamowienia.id_zamowienia=koszyk.id_zamowienia and imie like '%a'";
 $result = mysqli_query($conn,$sql);
 echo("<table border='1'>");
 echo("<th>suma produktuf</th>");
