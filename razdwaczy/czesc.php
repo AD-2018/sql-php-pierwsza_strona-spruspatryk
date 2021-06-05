bda7b42e0f3b3a514bd6e268b4bd63500fd35a6b
PatrykSprus <sprus1.patryk@gmail.com>
2021-06-05 15:12:34 +0200

    1

--- razdwaczy/czesc.php
+++ razdwaczy/czesc.php
@@ -1,16 +1,15 @@
 <?php
-require_once("connect.php");
+require_once("connect.php")
 $sql = "SELECT sum(ilosc) from koszyk,klienci,zamowienia where zamowienia.id_klient=klienci.id_klient and zamowienia.id_zamowienie=koszyk.id_zamowienie and imie like '%a'";
 $result = mysqli_query($conn,$sql);
 echo("<table border='1'>");
 echo("<th>suma produktuf</th>");
-while($row=mysqli_fetch_assoc($result))
-{
+while($row = mysqli_fetch_assoc($result)){
     echo("<tr>");
     echo("<td>".$row['sum(ilosc)']."</td>");
     echo("</tr>");
 
+
 }
 echo("</table>");
 ?>
-suma produktuw kobiet w koszyku
